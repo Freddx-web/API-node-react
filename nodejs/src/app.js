38a98fe9275c2b90375376fe34b2b4e62711f759
Fredd <66976840+Freddx-web@users.noreply.github.com>
2022-08-11 15:42:17 -0400

    Update app.js

--- nodejs/src/app.js
+++ nodejs/src/app.js
@@ -3,33 +3,28 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
 var app = express();
 
 // Connect
 const { mysql_db } = require('./config/connectMysql.js');
-const { mongodb } = require('./config/mongodb.js')
+const { mongodb } = require('./config/mongodb.js');
 
-
+// Router
 const indexRouter = require('./routes/indexRouter');
 
-app.use('/auth', indexRouter);
-
 // Middlewares
+app.use('/auth', indexRouter);
 app.use(express.json());
 
-
+// Api in React
 app.get("/api", (req, res) => {
   res.json({ message: "Great Day!"});
 });
 
-
-// catch 404 and forward to error handler
-
+// Catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404)).json({ message: "ERORR: 404!" });
 });
-
 
 // error handler
 app.use(function(err, req, res, next) {
